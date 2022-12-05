ac4997bccd54522d48ce6d232461b1bd2c1e08a5
lokeshvasnik <lokeshvasnik2003@gmail.com>
2022-12-05 11:16:40 +0530

    Working on Api again

--- src/components/Quotes/Quote.jsx
+++ src/components/Quotes/Quote.jsx
@@ -1,31 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Quote.css';
 
 const Quote = () => {
   // * Calling api
 
-  const [quotes, setQuotes] = useState([]);
-  const [isLoading, setisLoading] = useState(false);
+  // const [quotes, setQuotes] = useState([]);
+  // const [isLoading, setisLoading] = useState(false);
 
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      const response = await fetch(
-        'https://api.quotable.io/random?maxLength=50'
-      );
+ 
+  // useEffect(() => {
+  //   const interval = setInterval(async () => {
+  //     const response = await fetch(
+  //       'https://api.quotable.io/random?maxLength=50'
+  //       );
+        
+  //       const data = await response.json();
 
-      const data = await response.json();
+  //       setQuotes(data);
+  //     }, 8000);
+      
+  //     return () => setisLoading(interval);
+  //   }, []);
 
-      setQuotes(data);
-    }, 8000);
 
-    return () => clearInterval(interval);
-
-  }, []);
 
   return (
     <div className="container my-4">
       <div className="box">
-        {setisLoading && <p>{quotes.content}</p>}
+        <p>Keep smiling, because life is a beautiful thing and there's so much to smile about.</p>
       </div>
     </div>
   );
